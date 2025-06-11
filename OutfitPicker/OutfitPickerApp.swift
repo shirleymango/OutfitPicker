b97570dfda013cd93dba7165a6dc90ee1dc983f5
Shirley <shirleyzhu@college.harvard.edu>
2025-06-10 21:21:20 -0500

    Initial Commit

---
+++ OutfitPicker/OutfitPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutfitPickerApp.swift
+//  OutfitPicker
+//
+//  Created by ZhuMacPro on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct OutfitPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
