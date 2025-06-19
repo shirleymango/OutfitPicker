c9e8b6524bc33d551bd87972fd82909276381ca4
Shirley <shirleyzhu@college.harvard.edu>
2025-06-18 18:11:21 -0700

    refactor: use closetViewModel.closet as single source of truth in MainTabView

--- OutfitPicker/MainTabView.swift
+++ OutfitPicker/MainTabView.swift
@@ -10,38 +10,37 @@ import SwiftUI
 struct MainTabView: View {
     @StateObject private var outfitsViewModel = OutfitsViewModel()
     @StateObject private var closetViewModel = ClosetViewModel()
-    @State private var clothingItems: [ClothingItem] = []
-    
-    let closetStorage = ClosetStorage()
     
     var body: some View {
         TabView {
             PickOutfitView(
-                tops: clothingItems.filter {$0.itemType.isTop},
-                bottoms: clothingItems.filter {$0.itemType.isBottom},
-                outfitsViewModel: outfitsViewModel)
+                tops: closetViewModel.closet.filter { $0.itemType.isTop },
+                bottoms: closetViewModel.closet.filter { $0.itemType.isBottom },
+                outfitsViewModel: outfitsViewModel
+            )
             .tabItem {
                 Image(systemName: "tshirt")
                 Text("Pick Outfit")
             }
-            
+
             OutfitsView(
-                tops: clothingItems.filter { $0.itemType.isTop },
-                bottoms: clothingItems.filter { $0.itemType.isBottom },
+                tops: closetViewModel.closet.filter { $0.itemType.isTop },
+                bottoms: closetViewModel.closet.filter { $0.itemType.isBottom },
                 viewModel: outfitsViewModel
             )
             .tabItem {
                 Image(systemName: "hanger")
                 Text("Outfits")
             }
+
             ClosetView(viewModel: closetViewModel)
             .tabItem {
                 Image(systemName: "suitcase")
                 Text("Closet")
             }
         }
-        .onAppear {
-            clothingItems = closetStorage.load()
+        .task {
+            await closetViewModel.loadCloset()
         }
     }
 }
